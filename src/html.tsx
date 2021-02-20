40fc0568a5170b07508ee0115ae8a37f532723d2
ourfor <kiss@ourfor.top>
2021-02-20 17:01:17 +0800

    :fire: remove favicon in html
    use website default icon

--- src/html.tsx
+++ src/html.tsx
@@ -42,7 +42,6 @@ export function Html({ body, title, style }: HtmlProps) {
                 <meta charSet="UTF-8" />
                 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
                 <meta name="Description" content="秘密花园" />
-                <link rel="icon" type="image/svg+xml" href="//static.ourfor.top/www/drive/finder.svg" />
                 <link rel="stylesheet" href="//static.ourfor.top/fonts/font.css" />
                 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
                 <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no" />
