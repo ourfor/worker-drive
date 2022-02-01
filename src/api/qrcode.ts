5dab1694bdc82f7c1d9fdd61d5dfb59659bdcced
梁甜 <ourfor@qq.com>
2022-02-01 13:39:29 +0800

    :sparkles: add qrcode options

--- src/api/qrcode.ts
+++ src/api/qrcode.ts
@@ -4,10 +4,11 @@ export async function qrcode(req: Request): Promise<Response> {
     const params = new URL(req.url).searchParams;
     const size = params.get("size") ?? "1024"
     const data = params.get("data") ?? "Hello World"
+    const type = params.get("type") == 'utf8' ? 'utf8' : 'svg'
     let exception = ""
     let image = ""
     await qr.toString(data, { 
-        type: 'svg',
+        type,
         errorCorrectionLevel: "H",
         width: parseInt(size)
     }, (error, data) => {
